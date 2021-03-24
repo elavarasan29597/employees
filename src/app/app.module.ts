76ee0a2f94a46881a144ddddda5f4103e46bddcf
ElavarasanSiluvaimanickam <elavarasan.siluvaimanickam@qedtek.com>
2021-03-24 17:41:00 +0530

    my first git commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { ApiService } from './service/api.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+      { path: 'create-employee', component: EmployeeCreateComponent },
+      { path: 'edit-employee/:id', component: EmployeeEditComponent },
+      { path: 'employees-list', component: EmployeeListComponent }  
+    ])
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
